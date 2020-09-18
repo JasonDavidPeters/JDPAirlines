74c972aef8f4cb1bbed587e422c27b1a47a7ecf6
Jason Peters <iamjasonpeters@live.co.uk>
2020-09-18 08:07:42 -0700

    * Scene switching in works
    * Hyper links pointed in the right direction
    
    * Created super class to handle sub class scenes

--- JDPAirlines/src/com/jasondavidpeters/JDPAirlines/Main.java
+++ JDPAirlines/src/com/jasondavidpeters/JDPAirlines/Main.java
@@ -1,5 +1,7 @@
 package com.jasondavidpeters.JDPAirlines;
 
+import com.jasondavidpeters.JDPAirlines.gui.AccountScene;
+import com.jasondavidpeters.JDPAirlines.gui.CustomScene;
 import com.jasondavidpeters.JDPAirlines.gui.HomeScene;
 
 import javafx.application.Application;
@@ -20,6 +22,10 @@ public class Main extends Application {
     private static final double MAX_WIDTH = 1600; // get users max resolution width
     private static final double MIN_WIDTH = 800;
     public static final String APPLICATION_NAME = "JDP Airlines";
+    private double posX, posY;
+    private AccountScene accountScene;
+    private HomeScene homeScene;
+    private Stage stage;
 
     public static void main(String[] args) {
 	launch(args);
@@ -28,74 +34,90 @@ public class Main extends Application {
     @SuppressWarnings("exports")
     @Override
     public void start(Stage stage) throws Exception {
-
-//	BorderPane.setAlignment(image, Pos.TOP_CENTER);
-	BorderPane bp = new BorderPane();
-	stage.setMinWidth(MIN_WIDTH);
-	stage.setMinHeight(MIN_HEIGHT);
+	this.stage = stage;
+	
+	stage.setWidth(MIN_WIDTH);
+	stage.setHeight(MIN_HEIGHT);
 	screenWidth = stage.getWidth();
 	screenHeight = stage.getHeight();
 	stage.setTitle(APPLICATION_NAME);
-	HomeScene homeScene = new HomeScene(bp, MIN_WIDTH, MIN_HEIGHT);
+	
+	homeScene = new HomeScene(this, new BorderPane(), screenWidth, screenHeight);
+	accountScene = new AccountScene(this, new BorderPane(), screenWidth, screenHeight);
+
 	stage.setScene(homeScene);
-	stage.setMaximized(true);
+	
 	stage.show();
 
 	stage.widthProperty().addListener(e -> {
 	    if (stage.getWidth() >= MAX_WIDTH) {
 		screenWidth = MAX_WIDTH;
 		stage.setWidth(screenWidth);
-		resizeNodes(homeScene);
+		resizeNodes(stage.getScene());
 		return;
 	    }
 	    if (stage.getWidth() <= MIN_WIDTH) {
 		screenWidth = MIN_WIDTH;
 		stage.centerOnScreen();
 		stage.setWidth(screenWidth);
-		resizeNodes(homeScene);
+		resizeNodes(stage.getScene());
 		return;
 	    }
 	    screenWidth = stage.getWidth();
 	    stage.setWidth(screenWidth);
-	    resizeNodes(homeScene);
+	    resizeNodes(stage.getScene());
 	});
-
 	stage.heightProperty().addListener(e -> {
 	    if (stage.getHeight() >= MAX_HEIGHT) {
 		screenHeight = MAX_HEIGHT;
 		stage.setHeight(screenHeight);
-		resizeNodes(homeScene);
+		resizeNodes(stage.getScene());
 		return;
 	    }
 	    if (stage.getHeight() <= MIN_HEIGHT) {
 		screenHeight = MIN_HEIGHT;
 		stage.centerOnScreen();
 		stage.setHeight(screenHeight);
-		resizeNodes(homeScene);
+		resizeNodes(stage.getScene());
 		return;
 	    }
 	    screenHeight = stage.getHeight();
 	    stage.setHeight(screenHeight);
-	    resizeNodes(homeScene);
+	    resizeNodes(stage.getScene());
 	});
     }
 
-    @SuppressWarnings("exports")
     public void resizeNodes(Scene scene) {
-	((HomeScene) scene).getImage().setFitWidth((screenWidth / 2));
-	((HomeScene) scene).getImage().setFitHeight((screenHeight / 2));
-	((HomeScene) scene).getSearchBox().setPrefWidth((screenWidth / 2));
-	((HomeScene) scene).getRoundTripsBox().setPrefWidth((screenWidth / 2));
-	((HomeScene) scene).setFont(new Font(((HomeScene) scene).getFont().getName(), 0.03 * screenWidth));
-	ObservableList<Node> f = ((HomeScene) scene).getNavbar().getChildren();
-
-	for (Node n : f) {
-	    if (n instanceof Hyperlink) {
-		Hyperlink newLink = (Hyperlink) n;
-		newLink.setFont(((HomeScene) scene).getFont());
-		n = newLink;
+	if (scene instanceof HomeScene) {
+	    ((HomeScene) scene).getImage().setFitWidth((screenWidth / 2));
+	    ((HomeScene) scene).getImage().setFitHeight((screenHeight / 2));
+	    ((HomeScene) scene).getSearchBox().setPrefWidth((screenWidth / 2));
+	    ((HomeScene) scene).getRoundTripsBox().setPrefWidth((screenWidth / 2));
+	}
+	if (scene instanceof CustomScene) {
+	    ((CustomScene) scene).setFont(new Font(((CustomScene) scene).getFont().getName(), 0.03 * screenWidth));
+	    ObservableList<Node> f = ((CustomScene) scene).getNavbar().getChildren();
+	    for (Node n : f) {
+		if (n instanceof Hyperlink) {
+		    Hyperlink newLink = (Hyperlink) n;
+		    newLink.setFont(((CustomScene) scene).getFont());
+		    n = newLink;
+		}
 	    }
 	}
+
+    }
+
+    public AccountScene getAccountScene() {
+	return accountScene;
+    }
+
+    public HomeScene getHomeScene() {
+	return homeScene;
+    }
+
+    public Stage getStage() {
+	return this.stage;
     }
 
 }
--- JDPAirlines/src/com/jasondavidpeters/JDPAirlines/gui/HomeScene.java
+++ JDPAirlines/src/com/jasondavidpeters/JDPAirlines/gui/HomeScene.java
@@ -1,159 +1,122 @@
 package com.jasondavidpeters.JDPAirlines.gui;
 
+import com.jasondavidpeters.JDPAirlines.Main;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 
-public class HomeScene extends Scene {
-    
+public class HomeScene extends CustomScene {
+
     private ImageView image;
     private TextField searchBox;
     private TextField roundTripSearchBox;
-    private Font font;
-    private VBox navigationBar;
 
-    public HomeScene(Parent layout, double width, double height) {
-	super(layout, width, height);
-	//window - stage
-	//contents - scene
+    public HomeScene(Main main, Parent layout, double width, double height) {
+	super(main, layout, width, height);
+	// window - stage
+	// contents - scene
 
-	font = new Font(null, 0.05*width);
 	ToggleGroup radioBtns = new ToggleGroup();
-	
+
 	RadioButton oneWay = new RadioButton("One Way");
 	RadioButton roundTrip = new RadioButton("Round Trip");
-	
-	/*
-	 * TODO Once you click round trip radio button
-	 * create textfield below previous textfield for return flights
-	 */
-	
+
 	oneWay.setToggleGroup(radioBtns);
 	roundTrip.setToggleGroup(radioBtns);
-	
+
 	Image img = new Image("/logo.png");
-	
-	searchBox = new TextField("Search...");
-	roundTripSearchBox = new TextField("Round Trip...");
-	
+
+	searchBox = new TextField();
+	searchBox.setPromptText("Search..."); // greyed out promp text
+	roundTripSearchBox = new TextField("");
+
 	image = new ImageView(img);
 	image.setPreserveRatio(true);
 	Button searchButton = new Button("O");
 	Button invisBtn = new Button("O");
-	
+
 	invisBtn.setVisible(false);
-	
-	
-	navigationBar = new VBox();
+
+	getHomeLink().setOnAction(e -> {
+	    main.getStage().setScene(this);
+	});
+	getMyAccountLink().setOnAction(e -> {
+	    main.getStage().setScene(main.getAccountScene());
+	});
+
 	VBox centerGroup = new VBox(5); // Vertical grouping
 	HBox searchBar = new HBox(); // Horizontal grouping of search bar and search button
 	HBox roundTripBar = new HBox();
 	HBox flightOptions = new HBox(5);
-	
-	Hyperlink homeLink = new Hyperlink("Home");
-	homeLink.setFont(font);
-	
-	Hyperlink myAccount = new Hyperlink("My Account");
-	myAccount.setFont(font);
-	
-	Hyperlink myFlights = new Hyperlink("My Flights");
-	myFlights.setFont(font);
-	
+
 	/*
-	 * TODO: minimize the gap between the navigation bar
-	 * and logo
+	 * TODO: minimize the gap between the navigation bar and logo
 	 */
-	
-	
-	navigationBar.getChildren().add(homeLink);
-	navigationBar.getChildren().add(new Separator());
-	navigationBar.getChildren().add(myAccount);
-	navigationBar.getChildren().add(new Separator());
-	navigationBar.getChildren().add(myFlights);
-	navigationBar.getChildren().add(new Separator());
 
 	searchBox.setPrefWidth(width);
 	roundTripSearchBox.setPrefWidth(width);
-	
+
 	searchBar.getChildren().add(searchBox); // Add [----] the box to the bar
-	searchBar.getChildren().add(searchButton); //Add the search button to the bar
+	searchBar.getChildren().add(searchButton); // Add the search button to the bar
 	roundTripBar.getChildren().add(roundTripSearchBox);
 	roundTripBar.getChildren().add(invisBtn);
-	
+
 	flightOptions.getChildren().add(oneWay);
 	flightOptions.getChildren().add(roundTrip);
 	centerGroup.getChildren().add(image); // Add the logo to the logo bar group
-	
+
 	centerGroup.getChildren().add(flightOptions);
 	centerGroup.getChildren().add(searchBar); // Add the search bar group to the logo bar group
-	
+
 	searchBar.setAlignment(Pos.CENTER);
 	roundTripBar.setAlignment(Pos.CENTER);
-	
+
 	centerGroup.setAlignment(Pos.CENTER);
 	flightOptions.setAlignment(Pos.CENTER);
-	
+
 	BorderPane.setAlignment(centerGroup, Pos.CENTER);
 	((BorderPane) layout).setCenter(centerGroup);
-	((BorderPane)layout).setLeft(navigationBar); // Put the navigation bar to the left of the application
-	
+	// Put the navigation bar to the left of the application
+
 	centerGroup.setMaxWidth(width);
-	centerGroup.setPadding(new Insets(0, 0, 0, -width/4)); // top, right, bottom, left
-	
-//	navigationBar.setBorder(new Border(new BorderStroke(Color.BLACK, 
+	centerGroup.setPadding(new Insets(0, 0, 0, -width / 4)); // top, right, bottom, left
+
+//	getNavbar().setBorder(new Border(new BorderStroke(Color.BLACK, 
 //	            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 //	centerGroup.setBorder(new Border(new BorderStroke(Color.BLACK, 
 //	            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-	
+
 	roundTrip.selectedProperty().addListener(e -> {
 	    if (roundTrip.isSelected()) {
 		centerGroup.getChildren().add(roundTripBar);
-	    }
-	    else if (!roundTrip.isSelected()) {
+	    } else if (!roundTrip.isSelected()) {
 		centerGroup.getChildren().remove(roundTripBar);
 	    }
-	    
+
 	});
-	
-	
+
     }
-    
+
     public ImageView getImage() {
 	return this.image;
     }
+
     public TextField getSearchBox() {
 	return this.searchBox;
     }
+
     public TextField getRoundTripsBox() {
 	return this.roundTripSearchBox;
-    }
-    public Font getFont() {
-	return this.font;
-    }
-    public void setFont(Font font) {
-	this.font=font;
-    }
-    public VBox getNavbar() { 
-	return this.navigationBar;
     }
 
 }
